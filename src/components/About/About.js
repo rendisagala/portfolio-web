3553b60aa4d2f18ff4a74646cfdbab30b64c7ef3
rendisagala <rendisagala19@gmail.com>
2022-07-26 15:48:33 +0700

    update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,6 @@
 import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import InstagramIcon from '@material-ui/icons/Instagram'
 import { about } from '../../portfolio'
 import './About.css'
 
@@ -33,6 +34,8 @@ const About = () => {
                 href={social.github}
                 aria-label='github'
                 className='link link--icon'
+                target='_blank'
+                rel='noreferrer'
               >
                 <GitHubIcon />
               </a>
@@ -43,8 +46,22 @@ const About = () => {
                 href={social.linkedin}
                 aria-label='linkedin'
                 className='link link--icon'
+                target='_blank'
+                rel='noreferrer'
               >
                 <LinkedInIcon />
+              </a>
+            )}
+
+            {social.instagram && (
+              <a
+                href={social.instagram}
+                aria-label='instagram'
+                className='link link--icon'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <InstagramIcon />
               </a>
             )}
           </>
