6dd8575f9b8641e83fb7e69f4003d1bd2c7c1cdc
rendisagala <rendisagala19@gmail.com>
2022-07-26 15:53:23 +0700

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,12 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     Copyright © 2022 All Rights Reserved by
-    <a href='https://github.com/rendisagala' className='link footer__link'>
+    <a
+      href='https://github.com/rendisagala'
+      className='link footer__link'
+      target='_blank'
+      rel='noreferrer'
+    >
       @rendisagala.
     </a>
   </footer>
